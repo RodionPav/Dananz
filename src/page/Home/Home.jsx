76fcd72732b44801825671e3798d62ada74ecab7
Rodion Pavlovkii <rodionpavlovscky@yandex.ru>
2024-06-23 00:40:36 +0300

    Add intro in home

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -34,7 +34,31 @@ function Home() {
         </nav>
       </header>
       <div className="intro">
-        
+        <div className="intro__top">
+          <h1 className="intro__top__title">
+            Design your interor with high quality.
+          </h1>
+          <div className="into__top__card-wrapper">
+            <ul className="into__top__cards">
+              <li className="into__top__cards-item">
+                <span className="into__top__cards-item-span">350+</span> Project
+                Completed
+              </li>
+              <li className="into__top__cards-item">
+                <span className="into__top__cards-item-span">23+</span>
+                Professional Teams
+              </li>
+              <li className="into__top__cards-item">
+                <span className="into__top__cards-item-span">15+</span>Years
+                Experience
+              </li>
+            </ul>
+          </div>
+          <div className="into__top__text">
+            <p>2022 ALL RIGHT RESERVED</p>
+          </div>
+        </div>
+        <div className="intro__bottom" />
       </div>
       <div className="about"></div>
       <div className="service"></div>
