bd1bcdc552e4b0e3e58b366fc36071b9c0abb903
Rodion Pavlovkii <rodionpavlovscky@yandex.ru>
2024-06-26 14:57:08 +0300

    Creater header

---
+++ src/page/About/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <>
+      <div className="intro">
+        <h1 className="intro__title">About</h1>
+      </div>
+    </>
+  );
+}
+
+export default About;
--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -4,36 +4,6 @@ import "./Home.scss";
 function Home() {
   return (
     <>
-      <header className="header">
-        <a className="logo"> Dananz</a>
-        <nav className="header__menu">
-          <ul className="header__links__list">
-            <li className="header__links__list-item">
-              <a href="#" className="link header__link">
-                Home
-              </a>
-            </li>
-            <li className="header__links__list-item">
-              <a href="#" className="link header__link">
-                About Us
-              </a>
-            </li>
-            <li className="header__links__list-item">
-              <a href="#" className="link header__link">
-                Services
-              </a>
-            </li>
-            <li className="header__links__list-item">
-              <a href="#" className="link header__link">
-                Our Teams
-              </a>
-            </li>
-            <li className="header__links__list-item">
-              <button className="button header__button">Contact Us</button>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <div className="intro">
         <h1 className="intro__title">Design your interor with high quality.</h1>
         <div className="intro__text">
